06875a12eda036c9c6144477ef22d1a42c76b0d6
cherakaevs <marble462@gmail.com>
2022-03-16 09:47:01 +0400

    some changes

--- lab1/main.py
+++ lab1/main.py
@@ -1,15 +1,17 @@
 import sys
+import equalization
+import lc
+import thresholding
 
 from matplotlib import pyplot as plt
 from skimage.io import imshow, show, imread
 
-import thresholding, lc, equalization
-
 PATH = "/Users/karmikfeels/PycharmProjects/mmoi_labs/lab1/09_lena2.tif"
 
 
-def output(img, proc_img):
+def output(img, proc_img, title):
     fig = plt.figure(figsize=(10, 6))
+    plt.title(title)
     fig.add_subplot(2, 2, 1)
     imshow(img)
     fig.add_subplot(2, 2, 2)
@@ -31,19 +33,23 @@ def output(img, proc_img):
 def run(mode):
     img = imread(PATH)
     out_img = ''
+    t = ''
 
     if mode == "THRESHOLD":
         out_img = thresholding.thresholding(img)
+        t = "Пороговая обработка"
     if mode == "CONTRAST":
         out_img = lc.linear_contrast(img, img.min(), img.max())
+        t = "Линейное контрастирование"
     if mode == "EQUALIZE":
         out_img = equalization.equalize(img, 0, 255)
+        t = "Эквализация гистограмм"
 
-    output(img, out_img)
+    output(img, out_img, title=t)
 
 
 if __name__ == "__main__":
     run("THRESHOLD")
-    run("CONTRAST")
-    run("EQUALIZE")
+    # run("CONTRAST")
+    # run("EQUALIZE")
     sys.exit()
